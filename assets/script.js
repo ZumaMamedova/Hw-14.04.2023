995cd7e86cafd6411384d65e2e0cddbcdf21bdcc
ZumaMamedova <zumrudam@code.edu.az>
2023-04-14 23:48:33 +0400

    axios

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+
+
+const form=document.getElementById('form');
+form.addEventListener('submit', async function(e){
+e.preventDefault();
+const formData=new FormData(form);
+formData.append('first-name',document.getElementById('first-name').value);
+formData.append('surname',document.getElementById('surname').value);
+formData.append('id',1);
+console.log([...form]);
+
+try{
+    const res = await axios.post('https://httpbin.org/post',formData)
+    console.log(res);
+} catch (e) {
+console.log(error);
+}
+
+
+// .then(res =>console.log(res))
+// .catch(err =>console.log(err))
+
+
+});
+
+
+// let forms=document.querySelector(".form-data");
+
+// forms.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     let elem = e.target
+
+//     let formData={
+//         name: elem.querySelector('[name=first-name]').value,
+//         surmame: elem.querySelector('[surname="surname"]').value,
+//     };
+
+//     axios.post('/user',{
+//     firstName: 'Zulya',
+//     surName: 'Aliyeva',
+//     })
+        
+   
+
+// })
+
+// axios.post('/user',{
+//     firstName:
+//     surName:
+// })
+
+// .then(function(response){
+//     console.log(response);
+// })
+
+// .catch(function(error){
+//     console.log(error);
+// })
