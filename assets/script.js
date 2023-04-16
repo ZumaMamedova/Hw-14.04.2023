4417e1c56d10bd166dce4b3eaa2565a8a67fd1ea
ZumaMamedova <zumrudam@code.edu.az>
2023-04-16 12:52:15 +0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 
 
-const form=document.getElementById('form');
-form.addEventListener('submit', async function(e){
-e.preventDefault();
-const formData=new FormData(form);
-formData.append('first-name',document.getElementById('first-name').value);
-formData.append('surname',document.getElementById('surname').value);
-formData.append('id',1);
-console.log([...form]);
+// const form=document.getElementById('form');
+// form.addEventListener('submit', async function(e){
+// e.preventDefault();
+// const formData=new FormData(form);
+// formData.append('first-name',document.getElementById('first-name').value);
+// formData.append('surname',document.getElementById('surname').value);
+// formData.append('id',1);
+// console.log([...form]);
 
-try{
-    const res = await axios.post('https://httpbin.org/post',formData)
-    console.log(res);
-} catch (e) {
-console.log(error);
-}
+// try{
+//     const res = await axios.post('https://httpbin.org/post',formData)
+//     console.log(res);
+// } catch (e) {
+// console.log(error);
+// }
 
 
 // .then(res =>console.log(res))
 // .catch(err =>console.log(err))
 
 
-});
+// });
 
 
 // let forms=document.querySelector(".form-data");
@@ -56,3 +56,7 @@ console.log(error);
 // .catch(function(error){
 //     console.log(error);
 // })
+
+axios.post("https://jsonplaceholder.typicode.com/todos/",
+ {userId: 1111, id: 111315, title: "guide to the galaxy", completed: false})
+.then(function(response) {console.log(response);});
